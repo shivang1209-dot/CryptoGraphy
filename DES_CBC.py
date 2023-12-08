93a207888f592322e158f6ffd2e7488d8062add9
Shivang Tiwari <114579764+shivang1209-dot@users.noreply.github.com>
2023-12-09 01:25:33 +0530

    Add files via upload

---
+++ DES_CBC.py
@@ -0,0 +1,34 @@
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import pad,unpad
+from Crypto.Random import get_random_bytes
+
+def DES_CBC_encrypt(plaintext, DES_key, initialization_vector):
+    DES_CBC_cipher = DES.ne(DES_key, DES.MODE_CBC, iv=initialization_vector)
+
+    ciphertext = DES_CBC_cipher.encrypt(pad(plaintext, DES.block_size))
+
+    return ciphertext
+
+def DES_CBC_decrypt(ciphertext, DES_key, initialization_vector):
+
+    DES_CBC_cipher = DES.new(DES_key, DES.MODE_CBC, iv=initialization_vector)
+    decrypted_plaintext = unpad(DES_CBC_cipher(ciphertext), DES.block_size)
+
+    return decrypted_plaintext
+
+def main():
+
+    plaintext = input("Enter Plaintext: ").encode()
+
+    DES_key = get_random_bytes(8)
+    initialization_vector = get_random_bytes(8)
+
+    ciphertext = DES_CBC_encrypt(plaintext, DES_key, initialization_vector)
+    decrypted_plaintext = DES_CBC_decrypt(ciphertext, DES_key, initialization_vector)
+
+    print("Plaintext: ", plaintext.decode())
+    print("Ciphertext: ", ciphertext.hex())
+    print("Decrypted Plaintext: ", decrypted_plaintext.decode())
+
+if __name__ == "__main__":
+    main()
---
+++ DES_CTR.py
@@ -0,0 +1,34 @@
+from Crypto.Cipher import DES
+from Crypto.Random import get_random_bytes
+
+def DES_CTR_encrypt(plaintext, DES_key, nonce):
+    DES_CTR_cipher = DES.new(DES_key, DES.MODE_CTR, nonce=nonce)
+    ciphertext =  DES_CTR_cipher.encrypt(plaintext)
+
+    return ciphertext
+
+def DES_CTR_decrypt(ciphertext, DES_key, nonce):
+    DES_CTR_cipher = DES.neww(DES_key, DES.MODE_CTR, nonce=nonce)
+    decrypted_plaintext = DES_CTR_cipher.decrypt(ciphertext)
+
+    return decrypted_plaintext
+
+
+def main():
+    plaintext = input("Enter Plaintext: ").encode()
+
+    DES_key = get_random_bytes(8)
+
+    nonce = get_random_bytes(7)
+
+    ciphertext = DES_CTR_encrypt(plaintext, DES_key, nonce)
+
+    decrypted_plaintext = DES_CTR_decrypt(ciphertext, DES_key, nonce)
+
+    print("Plaintext: ", plaintext.decode())
+    print("Ciphertext: ", ciphertext.hex())
+    print("Decrypted Plaintext: ", decrypted_plaintext.decode())
+
+if __name__ == "__main__":
+
+    main()
